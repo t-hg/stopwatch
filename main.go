b20d2bee08724302ef24268fcb4701179d710ba1
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2024-05-04 05:24:54 +0200

    refactoring

--- main.go
+++ main.go
@@ -15,23 +15,9 @@ const startupMessage = `'s' - start/stop
 'r' - reset
 'q' - quit`
 
-func initialize() {
-	curses.InitScr()
-	curses.Cbreak()
-	curses.NoEcho()
-	curses.CursSet(0)
-	curses.NoDelay(true)
-}
-
-func cleanup() {
-	curses.CursSet(2)
-	curses.Echo()
-	curses.NoCbreak()
-	curses.EndWin()
-}
-
-func print(text string) {
-	curses.Clear()
+func render(text string) {
+	// find y, x so that given
+	// text is centered
 	maxLineLen := 0
 	lines := strings.Split(text, "\n")
 	for _, line := range lines {
@@ -43,6 +29,9 @@ func print(text string) {
 	maxX := curses.GetMaxX()
 	y := maxY/2 - len(lines)/2
 	x := maxX/2 - maxLineLen/2
+
+	// print lines respectively
+	curses.Clear()
 	for idx, line := range lines {
 		curses.MvAddStr(y+idx, x, line)
 	}
@@ -50,17 +39,30 @@ func print(text string) {
 }
 
 func main() {
-	initialize()
+	// setup
+	curses.InitScr()
+	curses.Cbreak()
+	curses.NoEcho()
+	curses.CursSet(0)
+	curses.NoDelay(true)
+
+	// signal handlers
 	sigint := make(chan os.Signal, 1)
 	sigwinch := make(chan os.Signal, 1)
 	signal.Notify(sigint, syscall.SIGINT)
 	signal.Notify(sigwinch, syscall.SIGWINCH)
+
+	// text to be displayed
 	text := startupMessage
+
+	// loop variables
 	var running bool
 	var start int64
 	var elapsed int64
+
 loop:
 	for {
+		// handle signals
 		select {
 		case <-sigint:
 			break loop
@@ -69,6 +71,8 @@ loop:
 			curses.Refresh()
 		default:
 		}
+
+		// handle character input
 		switch curses.GetCh() {
 		case 's':
 			if running {
@@ -88,14 +92,26 @@ loop:
 		case 'q':
 			break loop
 		}
+
+		// update watch
 		if running {
 			now := time.Now().UnixMilli()
 			elapsed = now - start
 			text = fmt.Sprintf("%d", elapsed)
 		}
+
+		// display text
 		// TODO: styling
-		print(text)
+		render(text)
+
+		// little time interval
+		// to avoid busy wait
 		time.Sleep(50 * time.Millisecond)
 	}
-	cleanup()
+
+	// cleanup
+	curses.CursSet(2)
+	curses.Echo()
+	curses.NoCbreak()
+	curses.EndWin()
 }
